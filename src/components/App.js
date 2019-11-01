4446d352764baf20fef0073cfb1422a4aec3970e
Simon Georgiev <52510040+simeong-web@users.noreply.github.com>
2019-11-01 12:44:10 +0000

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { selectSong } from '../actions/index';
+
+const App = () => {
+    return (
+        <div>Hi</div>
+    )
+};
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+import { combineReducers } from 'redux';
+
+
+const songReducer = () => {
+    return [
+        { title: 'No Scrubs', duration: '4:05' },
+        { title: 'Macarena', duration: '2:30' },
+        { title: 'All star', duration: '3:15' },
+        { title: 'I want it that way', duration: '1:45' }
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SELECTED_SONG') {
+        return action.payload;
+    }
+    
+    return selectedSong;
+};
+
+export default combineReducers({
+    songs: songReducer,
+    selectedSong: selectedSongReducer
+})
